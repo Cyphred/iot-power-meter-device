8c1fdc1184c82e8bc02b9c22a1806ae13cc5e9ee
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-10 22:34:25 +0800

    Removes web server from device api

--- device_api/src/index.ts
+++ device_api/src/index.ts
@@ -1,7 +1,5 @@
 import dotenv from "dotenv";
-import express from "express";
-import mongoose, { mongo } from "mongoose";
-import cors from "cors";
+import mongoose from "mongoose";
 
 // Loads .env data
 dotenv.config();
@@ -15,32 +13,4 @@ const mongoUri = process.env.MONGO_URI
 // Connect to db
 await mongoose.connect(mongoUri);
 
-// Create express app
-const app = express();
-
-// Enables parsing of incoming data into json
-app.use(express.json());
-
-// Logs incoming requests to console
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
-
-// IMPORTANT NOTE
-// Change the origin to the domain during production
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-// Load in root router
-// app.use("/api", rootRouter);
-
-// Start listening for requests
-app.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
-});
-
-export default app;
+console.log(`Connected to mongodb at ${mongoUri}`);
