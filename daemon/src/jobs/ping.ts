cec81989778fd4a4cac1973f1e7f6b4ea27404a1
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-11 11:27:04 +0800

    Adds sensor error and load connected reporting to ping

--- daemon/src/jobs/ping.ts
+++ daemon/src/jobs/ping.ts
@@ -5,9 +5,21 @@ import getRedisClient from "../util/getRedisClient.js";
 
 export default async () => {
   const redisClient = await getRedisClient();
+
+  const sensorError = await redisClient.get("SENSOR_ERROR");
+  const loadConnected = await redisClient.get("LOAD_CONNECTED");
+
   // Send report data to the backend
   try {
-    const response = await axios.get(`${process.env.PING_URI}`);
+    const response = await axios.post(
+      `${process.env.PING_URI}`,
+      {
+        sensorError: sensorError === "1" ? true : undefined,
+        loadConnected: loadConnected === "1" ? true : false,
+      },
+      { headers: { Authorization: process.env.METER_SECRET } }
+    );
+
     let connected = 0;
     if (response.status === 200) connected = 1;
     redisClient.set("SERVER_ONLINE", connected);
