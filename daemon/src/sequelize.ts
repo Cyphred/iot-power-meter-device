0cb1c799e9e515d92fee9679e829e388159419cb
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-12 03:10:34 +0800

    Adds sequelize initializer

---
+++ daemon/src/sequelize.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Sequelize } from "sequelize";
+
+let dbPort = parseInt(process.env.POSTGRES_PORT);
+// Defaults to 3306
+if (isNaN(dbPort)) dbPort = 3306;
+
+const dbHost = process.env.POSTGRES_HOST;
+const dbName = process.env.POSTGRES_NAME;
+const dbUser = process.env.POSTGRES_USER;
+const dbPassword = process.env.POSTGRES_PASS;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  port: dbPort,
+  dialect: "mysql",
+  define: { timestamps: true },
+});
+
+export default sequelize;
