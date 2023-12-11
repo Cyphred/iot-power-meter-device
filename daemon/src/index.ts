51e0214b7ab2657cc901eda05d714ce61273add6
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2023-12-12 04:14:55 +0800

    Adds redis connection testing

--- daemon/src/index.ts
+++ daemon/src/index.ts
@@ -3,6 +3,7 @@ import sendDataToServer from "./jobs/sendDataToServer.js";
 import aggregateDataFromRedis from "./jobs/aggregateDataFromRedis.js";
 import { scheduleJob } from "node-schedule";
 import ping from "./jobs/ping.js";
+import getRedisClient from "./util/getRedisClient.js";
 
 // Loads .env data
 dotenv.config();
@@ -10,6 +11,16 @@ dotenv.config();
 // Check if secret is present in env
 const secret = process.env.METER_SECRET;
 if (!secret) throw new Error("Meter secret not defined in .env");
+
+try {
+  console.log("Testing connection to redis...");
+  const redisClient = await getRedisClient();
+  console.log("Redis connection OK");
+  await redisClient.quit();
+} catch (err) {
+  console.error("Could not connect to redis");
+  throw err;
+}
 
 const startScheduledJobs = () => {
   const aggregateJob = scheduleJob("*/5 * * * * *", (fireDate) => {
